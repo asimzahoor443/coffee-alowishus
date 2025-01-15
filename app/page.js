6cf9b0b2acc8f1b2a121d064e60668d05da86dca
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2025-01-15 13:57:45 +0500

    feat:updated page.js, globallcss and layout.js and docs: added Header.jsx

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Header from './_components/Header';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <h1></h1>
+      <h1>Hello</h1>
+      <Header />
     </main>
   );
 }
