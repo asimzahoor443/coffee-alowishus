610fddb6654022373ab17150455b637e22abebd5
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2025-01-15 14:27:04 +0500

    docs: added assests file

--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
 import Header from './_components/Header';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello</h1>
-      <Header />
-    </main>
-  );
+  return <Header />;
 }
